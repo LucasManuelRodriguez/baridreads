6d62327582ac91dbfa644fa12136d9d3878f1b40
Lucas Rodriguez <rodriguez.m.lucas@gmail.com>
2022-01-31 17:21:00 -0300

    add cart to store

--- resources/views/components/show.blade.php
+++ resources/views/components/show.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.landing')
 @section('content2')
     @include('card')
+    <script src="./Album example · Bootstrap v5.1_files/bootstrap.bundle.min.js.descarga" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 @endsection
